c490ce72df81b526ca4b28383cf10cfc9ac0fe44
sibghataziz <sibghat.aziz98@gmail.com>
2022-10-09 12:25:24 +0530

    initial project1

--- jio_mart_clone/src/App.js
+++ jio_mart_clone/src/App.js
@@ -1,7 +1,13 @@
+import AllRoutes from "./Components/AllRoutes";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar/>
+      <AllRoutes/>
+      <Footer/>
     </div>
   );
 }
--- jio_mart_clone/src/index.js
+++ jio_mart_clone/src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <Provider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 );
-
