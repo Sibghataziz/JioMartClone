3d4ac883e3b214a9680cc49ffdae28a737bb126a
aniket-0703 <aniketmhalungekar0703@gmail.com>
2022-10-13 11:55:50 +0530

    add

---
+++ jio_mart_clone/cart-page/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import Basket from './components/cart/Basket';
+
+function App() {
+  return (
+    <div className="App">
+     <Basket/>
+    </div>
+  );
+}
+
+export default App;
