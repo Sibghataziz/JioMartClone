caa43ad7825c7114a130ad91b24e130be40ebb3f
MohdumaR125 <umar231294@gmail.com>
2022-10-14 00:50:40 +0530

    styling done

--- jio_mart_clone/src/Components/AllRoutes.jsx
+++ jio_mart_clone/src/Components/AllRoutes.jsx
@@ -2,13 +2,13 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AllProduct from "../Pages/AllProduct";
-import DummyHome from "./DummyHome";
+import Navbar from "./Navbar";
 
 export default function AllRoutes() {
   return (
     <Box>
       <Routes>
-        <Route path="/" element={<DummyHome />} />
+        {/* <Route path="/" element={<Navbar />} /> */}
         <Route path="/products/:product_category" element={<AllProduct />} />
       </Routes>
     </Box>
